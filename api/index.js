a14488b2231c7ca07fdbb0a67f875dca1d9941e4
suman2737 <yadavsuman27031999@gmail.com>
2025-04-19 21:23:11 +0530

    air-bnb-clone

--- api/index.js
+++ api/index.js
@@ -142,7 +142,7 @@ app.post('/api/upload-by-link', async (req,res) => {
 });
 
 const photosMiddleware = multer({dest:'/tmp'});
-app.post('/upload', photosMiddleware.array('photos', 100), async (req,res) => {
+app.post('/api/upload', photosMiddleware.array('photos', 100), async (req,res) => {
   const uploadedFiles = [];
   for (let i = 0; i < req.files.length; i++) {
     const {path,originalname,mimetype} = req.files[i];
